d1101bbd98d3a13da6a67ac01085c42b693a0a69
Semyon <sorokaty@rebels.ai>
2022-04-10 19:16:32 +0700

    bug fix and refactoring

--- ecriptors/asymmetry/asymmetry_module.py
+++ ecriptors/asymmetry/asymmetry_module.py
@@ -1,5 +1,4 @@
 import cryptography
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 
@@ -11,9 +10,8 @@ class Asymmetric_encriptor:
     def __init__(self, keypath: str) -> None:
         # private key
         private_key = rsa.generate_private_key(
-            public_exponent=3,
-            key_size=2048,
-            backend=default_backend()
+            public_exponent=65537,
+            key_size=4096,
         )
         public_key = private_key.public_key()
         
@@ -44,14 +42,12 @@ class Asymmetric_encriptor:
             return serialization.load_pem_private_key(
                 key_file.read(),
                 password=None,
-                backend=default_backend()
             )
     # ######### Public (shared) device only ##########
     def __read_public (self, keypath):
         with open(keypath + 'public.pem', "rb") as key_file:
             return serialization.load_pem_public_key(
                 key_file.read(),
-                backend=default_backend()
             )
     
 #     # make sure the following are imported
@@ -59,38 +55,43 @@ class Asymmetric_encriptor:
 # ######### Public (shared) device only #########
 
     def encrypt(self, filepath):
-        # data = '' 
-        # with open (filepath + 'text.txt') as f: 
-        #     data = f.read()
-        # data = data.encode('utf-8')
-        # data = data.split()
+        data = '' 
+        with open (filepath + 'text.txt') as f: 
+            data = f.read()
+        data = data.encode('utf-8')
+        data = data.split()
         data = [b'My secret weight', b'My secret id']
+        res = []
         open(filepath + 'encrypt/text.txt', "wb").close() # clear file
+        print(1)
         for encode in data:
             encrypted =  self.public_key.encrypt(
                 encode,
                 padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA1()),
-                    algorithm=hashes.SHA1(),
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
                     label=None
                 )
             )
+            print(encrypted)
             with open(filepath + 'encrypt/text.txt', "ab") as f: f.write(encrypted)
 
     def decrypt(self, filepath):
         read_data = []
-        with open(filepath + 'encrypt/text.txt', "rb") as f:
+        print(2)
+        with open(filepath + 'encrypt/text.txt', "rb") as f:  
             for encrypted in f:
+                print(encrypted)
                 read_data.append(
                     self.private_key.decrypt(
                         encrypted,
                     padding.OAEP(
-                        mgf=padding.MGF1(algorithm=hashes.SHA1()),
-                        algorithm=hashes.SHA1(),
+                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                        algorithm=hashes.SHA256(),
                         label=None
                     )))
-
-
+            print(read_data)
+        with open(filepath + 'decrypt/text.txt', "w") as f: f.write(str(read_data))
 
 
 # # make sure the following are imported
